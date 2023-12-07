846ca5d6a63604a21575f3b16f8958c58c8f2291
bcgkky <bcgkky1@gmail.com>
2023-12-07 13:21:01 +0300

    last
    ...

---
+++ Assets/sahneYonetim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sahneYonetim : MonoBehaviour
+{
+    public GameObject Loading;
+    public Slider yuklemeSlider;
+ 
+    public void SahneYukle (int LevelId)
+    {
+        StartCoroutine(SahneYuklemeAsamasi(LevelId));
+    }
+
+    IEnumerator SahneYuklemeAsamasi(int SceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneIndex);
+        Loading.SetActive(true);
+        
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            yuklemeSlider.value = progress;
+            Debug.Log("progress : " +progress);
+            Debug.Log("operation progress : " +operation.progress);
+            yield return null;
+        }
+        
+    }
+}
